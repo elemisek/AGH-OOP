2e0c92e5129b44f7a573afc830c93611f20ac2cb
Tomasz Kasprzak <50302254+elemisek@users.noreply.github.com>
2020-05-13 18:48:14 +0200

    Create FZlozona.h

---
+++ 4th-term/Lab1/FZlozona.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Fun.h"
+class FZlozona : public Fun
+{
+public:
+    // Simple constructor
+    FZlozona(Fun * fnc1, Fun * fnc2);
+    virtual ~FZlozona();
+    
+    // Calculate for given argument
+    double wartosc(double arg) const override;
+    
+    // Overridden clone function
+    virtual FZlozona* clone() const override;
+    
+private:
+    Fun * mfnc1 = nullptr;
+    Fun * mfnc2 = nullptr;
+};
