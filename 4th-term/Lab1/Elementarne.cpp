07d05648b53a362ce42ee309488250f6c99bf152
Tomasz Kasprzak <50302254+elemisek@users.noreply.github.com>
2020-05-13 18:45:16 +0200

    Create Elementarne.cpp

---
+++ 4th-term/Lab1/Elementarne.cpp
@@ -0,0 +1,65 @@
+#include "Elementarne.h"
+#include <cmath>
+Liniowa * Liniowa::utworz()
+{
+    return new Liniowa;
+}
+Liniowa * Liniowa::a(double a)
+{
+    ma=a;
+    return this;
+}
+Liniowa * Liniowa::b(double b)
+{
+    mb=b;
+    return this;
+}
+double Liniowa::wartosc(double arg) const
+{
+    return ma*arg+mb;
+}
+Liniowa * Liniowa::clone() const
+{
+    return new Liniowa(*this);
+}
+
+
+Kwadratowa * Kwadratowa::utworz()
+{
+    return new Kwadratowa;
+}
+Kwadratowa * Kwadratowa::a(double a)
+{
+    ma=a;
+    return this;
+}
+Kwadratowa * Kwadratowa::b(double b)
+{
+    mb=b;
+    return this;
+}
+Kwadratowa * Kwadratowa::c(double c)
+{
+    mc=c;
+    return this;
+}
+double Kwadratowa::wartosc(double arg) const
+{
+    return ma*arg*arg+mb*arg+mc;
+}
+Kwadratowa * Kwadratowa::clone() const
+{
+    return new Kwadratowa(*this);
+}
+
+
+
+
+double Sinus::wartosc(double arg) const
+{
+    return sin(arg);
+}
+Sinus * Sinus::clone() const
+{
+    return new Sinus(*this);
+}
